54d24ca5c92116064e7677d5c511a1ed782338fa
Dmytro Ukrainets <dmytro.army24@gmail.com>
2024-04-25 12:52:41 +0300

    Conect db to the project.

--- src/main/java/com/example/demo/controllers/CategoryController.java
+++ src/main/java/com/example/demo/controllers/CategoryController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Category;
+import com.example.demo.repo.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping("/category")
     public String categoryMain(Model model) {
+        Iterable<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
         return "category";
     }
 }
