41671308adfc0696c81098784c8fc64be5e1c684
Dmytro Ukrainets <dmytro.army24@gmail.com>
2024-04-26 18:10:43 +0300

    Add edit and delete functions to product.

--- src/main/java/com/example/demo/controllers/ProductsController.java
+++ src/main/java/com/example/demo/controllers/ProductsController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class ProductsController {
     @Autowired
@@ -43,5 +45,49 @@ public class ProductsController {
             productRepository.save(product);
             return "redirect:/products";
         }
+    }
+
+    @GetMapping("/products/edit")
+    public String productsEdit(Model model) {
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        Iterable<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "productsEdit";
+    }
+
+    @PostMapping("/products/edit")
+    public String productsPostEdit(@RequestParam Long id, @RequestParam Long id_category, @RequestParam String name,
+                                  @RequestParam String description, Model model) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setId_category(id_category);
+            product.setName(name);
+            product.setDescription(description);
+            productRepository.save(product);
+        } else {
+            return "redirect:/error-page";
+        }
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/delete")
+    public String productsDelete(Model model) {
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "productsDelete";
+    }
+
+    @PostMapping("/products/delete")
+    public String productsPostDelete(@RequestParam Long id, Model model) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            productRepository.delete(product);
+        } else {
+            return "redirect:/error-page";
+        }
+        return "redirect:/products";
     }
 }
